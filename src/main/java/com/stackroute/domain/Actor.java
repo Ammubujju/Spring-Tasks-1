16c57f6874831cab967926ac37f967c04d336ff3
gayathri <gayathrikv.199521@gmail~.com>
2019-07-11 23:50:00 +0530

    submitted

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -1,36 +1,33 @@
 package com.stackroute.domain;
 
-
-
 import java.util.*;
 
 public class Actor
 {
-    public String getName() {
-        return name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
     private String name;
     private String gender;
     private int age;
 
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void setGender(String gender)
+     public Actor(String name,String gender,int age)
+     {
+         this.name = name;
+         this.gender=gender;
+         this.age=age;
+
+     }
+
+    public String getName()
     {
-        this.gender=gender;
+        return name;
     }
-    public void setAge(int age)
+
+    public String getGender()
     {
-        this.age=age;
+        return gender;
+    }
+
+    public int getAge()
+    {
+        return age;
     }
 }
